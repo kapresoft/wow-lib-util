850e862c020dc500e9d05449b816c223ffa28946
Tony Lagnada <deloyz@gmail.com>
2024-05-22 17:36:32 -0700

    AceConfigUtil: new create methods

--- Lib/Util/AceConfigUtil.lua
+++ Lib/Util/AceConfigUtil.lua
@@ -19,7 +19,7 @@ local sformat = string.format
 New Instance
 -------------------------------------------------------------------------------]]
 --- @class Kapresoft_LibUtil_AceConfigUtil
-local S = LibStub:NewLibrary(ModuleName, 1); if not S then return end
+local S = LibStub:NewLibrary(ModuleName, 2); if not S then return end
 
 --[[-----------------------------------------------------------------------------
 Library Methods
@@ -94,6 +94,19 @@ do
 
     --- @param opt AceConfigOption
     --- @param localeKey string
+    function o:NameDesc(opt, localeKey)
+        local descKey = localeKey .. '::Desc'
+        if not self.L then
+            opt.name, opt.desc = localeKey, descKey
+            return
+        end
+
+        opt.name = self.L[localeKey]
+        opt.desc = self.L[descKey]
+    end
+
+    --- @param opt AceConfigOption
+    --- @param localeKey string
     function o:NameDescGlobal(opt, localeKey)
         local descKey = localeKey .. '::Desc'
         if not self.L then
@@ -115,12 +128,24 @@ do
         end
 
         opt.name = self.L[localeKey]
-        opt.desc = self.L.C(descKey)
+        opt.desc = self:C(descKey)
     end
 
     --- Create a Global Option
     --- @param localeKey string The locale key
     --- @param options AceConfigOption
+    --- @return AceConfigOption
+    function o:CreateOption(localeKey, options)
+        local option = {}; self:NameDesc(option, localeKey)
+        if options == nil or type(options) ~= 'table' then return option end
+        for k in pairs(option) do options[k] = option[k] end
+        return options
+    end
+
+    --- Create a Global Option
+    --- @param localeKey string The locale key
+    --- @param options AceConfigOption
+    --- @return AceConfigOption
     function o:CreateGlobalOption(localeKey, options)
         local option = {}; self:NameDescGlobal(option, localeKey)
         if options == nil or type(options) ~= 'table' then return option end
@@ -131,11 +156,54 @@ do
     --- Create a Character-Specific Option
     --- @param localeKey string The locale key
     --- @param options AceConfigOption
+    --- @return AceConfigOption
     function o:CreateCharOption(localeKey, options)
         local option = {}; self:NameDescCharacter(option, localeKey)
         if options == nil or type(options) ~= 'table' then return option end
         for k in pairs(option) do options[k] = option[k] end
         return options
     end
+
+    --- @param localeKey Name
+    --- @return AceConfigOption
+    function o:CreateLabel(localeKey, options)
+        return self:CreateLabelByName(self.L[localeKey], options)
+    end
+
+    --- @param name Name
+    --- @return AceConfigOption
+    function o:CreateLabelByName(name, options)
+        local option = { type = "description", width = 'full', fontSize = 'medium', name = name, }
+        if options == nil or type(options) ~= 'table' then return option end
+        for k in pairs(option) do if not options[k] then options[k] = option[k] end end
+        return options
+    end
+
+    --- ##### Usage:
+    --- ```
+    --- local sp = self:CreateSpacer()
+    --- local sp = self:CreateSpacer(1) -- order
+    --- local sp = self:CreateSpacer('__')
+    --- local sp = self:CreateSpacer('__', { order = 2, width=1.0 })
+    --- local sp = self:CreateSpacer({ order = 2, width=1.0 })
+    --- ```
+    --- @param spacerTextOrOrderOrOptions string|number|AceConfigOption|nil
+    --- @param options AceConfigOption|nil
+    --- @return AceConfigOption
+    function o:CreateSpacer(spacerTextOrOrderOrOptions, options)
+        local spacerText = ' '
+        local option = { type = "description", width = 'full', fontSize = 'medium', name = spacerText, }
+        if type(spacerTextOrOrderOrOptions) == 'string' then
+            option.name = spacerTextOrOrderOrOptions
+        elseif type(spacerTextOrOrderOrOptions) == 'number' then
+            option.order = spacerTextOrOrderOrOptions
+        elseif type(spacerTextOrOrderOrOptions) == 'table' then
+            options = spacerTextOrOrderOrOptions
+        end
+        if options == nil or type(options) ~= 'table' then return option end
+        for k in pairs(option) do if not options[k] then options[k] = option[k] end end
+        return options
+    end
+
 end
 
