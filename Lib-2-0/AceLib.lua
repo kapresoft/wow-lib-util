a6034eaeb96dfaece59f1a0ef689831b4039f947
Tony Lagnada <deloyz@gmail.com>
2026-04-11 18:40:40 -0700

    AceLib update to use dot instead of ":"

--- Lib-2-0/AceLib.lua
+++ Lib-2-0/AceLib.lua
@@ -11,15 +11,15 @@ setmetatable(S, S.mt)
 
 local o = S
 
-function o:AceAddon() return LibStub('AceAddon-3.0') end
-function o:AceBucket() return LibStub('AceBucket-3.0') end
-function o:AceConfig() return LibStub('AceConfig-3.0') end
-function o:AceConfigDialog() return LibStub('AceConfigDialog-3.0') end
-function o:AceConfigRegistry() return LibStub('AceConfigRegistry-3.0') end
-function o:AceConsole() return LibStub('AceConsole-3.0') end
-function o:AceDB() return LibStub('AceDB-3.0') end
-function o:AceDBOptions() return LibStub('AceDBOptions-3.0') end
-function o:AceEvent() return LibStub('AceEvent-3.0') end
-function o:AceGUI() return LibStub('AceGUI-3.0') end
-function o:AceHook() return LibStub('AceHook-3.0') end
-function o:AceLocale() return LibStub('AceLocale-3.0') end
+function o.AceAddon() return LibStub('AceAddon-3.0') end
+function o.AceBucket() return LibStub('AceBucket-3.0') end
+function o.AceConfig() return LibStub('AceConfig-3.0') end
+function o.AceConfigDialog() return LibStub('AceConfigDialog-3.0') end
+function o.AceConfigRegistry() return LibStub('AceConfigRegistry-3.0') end
+function o.AceConsole() return LibStub('AceConsole-3.0') end
+function o.AceDB() return LibStub('AceDB-3.0') end
+function o.AceDBOptions() return LibStub('AceDBOptions-3.0') end
+function o.AceEvent() return LibStub('AceEvent-3.0') end
+function o.AceGUI() return LibStub('AceGUI-3.0') end
+function o.AceHook() return LibStub('AceHook-3.0') end
+function o.AceLocale() return LibStub('AceLocale-3.0') end
