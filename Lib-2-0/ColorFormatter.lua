fb543dc6c81458cd3d1afa6dc2386a1e02feaaa6
Tony Lagnada <deloyz@gmail.com>
2026-04-19 15:23:34 -0700

    ColorFromHex fix

--- Lib-2-0/ColorFormatter.lua
+++ Lib-2-0/ColorFormatter.lua
@@ -10,7 +10,7 @@ VERSION:: Bump MINOR_VERSION whenever a change occurs
 -- 1: initial version
 
 -------------------------------------------------------------------------------]]
-local MAJOR, MINOR = 'Kapresoft-ColorFormatter-2-0', 1
+local MAJOR, MINOR = 'Kapresoft-ColorFormatter-2-0', 2
 
 --- @class Kapresoft-ColorFormatter-2-0
 local S = LibStub:NewLibrary(MAJOR, MINOR); if not S then return end
@@ -48,40 +48,42 @@ end
 
 --- ```
 --- Examples:
---- color = ColorFormatter:ColorFromHex('565656fc')
 --- color = ColorFormatter:ColorFromHex('565656')
 --- color = ColorFormatter:ColorFromHex('fc565656')
 ---
 --- print('Say:', color:WrapTextInColorCode('Hello'))  -- prints in color
 --- ```
---- @param hexColorRBA HexRGBA|HexRGB|HexARGB | '565656fc' | '565656' | 'fc565656'
+--- @param hexColor HexRGB|HexARGB | '565656' | 'fc565656'
 --- @return colorRGBA
-function o:ColorFromHex(hexColorRBA)
-  assertsafe(hexColorRBA, "ColorFromHex:hexColor): {hexColor} should be a string, but was [%s]",
-    type(hexColorRBA))
-  local color = self:_ColorFromHexARGB(hexColorRBA)
+function o:ColorFromHex(hexColor)
+  assertsafe(hexColor, "ColorFromHex(hexColorRBA): {hexColor} should be a string, but was [%s]",
+    type(hexColor))
+  local color = self:_ColorFromHexARGB(hexColor)
   if not color then
-    color = self:_ColorFromHexRGB(hexColorRBA)
+    color = self:_ColorFromHexRGB(hexColor)
   end
-  assert(type(color) == 'table', 'ColorFromHex:hexColor): {hexColor} is invalid: ' .. tostring(hexColorRBA))
+  assert(type(color) == 'table', 'ColorFromHex(hexColor): {hexColor} is invalid: ' .. tostring(hexColor))
   return color
 end
 
 --- @private
---- @param hexColorARGB HexARGB | 'fc565656'
+--- @param hexColorARGB? HexARGB | 'fc565656'
 function o:_ColorFromHexARGB(hexColorARGB)
-	return CreateColorFromHexString(hexColorARGB);
+  if #hexColorARGB == #o.COLOR_FORMAT_ARGB then
+    local a, r, g, b = ExtractColorValueFromHex(hexColorARGB, 1), ExtractColorValueFromHex(hexColorARGB, 3),
+        ExtractColorValueFromHex(hexColorARGB, 5), ExtractColorValueFromHex(hexColorARGB, 7);
+    return CreateColor(r, g, b, a);
+  end
+  return nil;
 end
-
 --- @private
 --- @param hexColor HexRGBA|HexRGB | '565656fc' | '565656'
 --- @return colorRGBA?
-function o:_ColorFromHexRGB(hexColor)
-	if #hexColor == #COLOR_FORMAT_RGBA then
-		return CreateColorFromRGBAHexString(hexColor);
-	elseif #hexColor == #COLOR_FORMAT_RGB then
-		return CreateColorFromRGBHexString(hexColor);
-	end
-	assertsafe(false, "CreateColorFromBestRGBHexString input must be hexadecimal digits in either of these formats: %s / %s", COLOR_FORMAT_RGBA, COLOR_FORMAT_RGB);
+function o:_ColorFromHexRGB(hexColorRGB)
+	if #hexColorRGB == #o.COLOR_FORMAT_RGB then
+  		local r, g, b = ExtractColorValueFromHex(hexColorRGB, 1), ExtractColorValueFromHex(hexColorRGB, 3),
+  		  ExtractColorValueFromHex(hexColorRGB, 5);
+  		return CreateColor(r, g, b, 1);
+  	end
 	return nil;
 end
